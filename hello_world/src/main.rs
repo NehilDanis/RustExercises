1979b4a51fff8a6f843b2e378e68e459c7d2141d
Nehil Danış <nehild@outlook.com>
2023-07-30 15:10:33 +0200

    ADDED: operators

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -4,7 +4,7 @@
 
 use std::mem;
 
-fn main() {
+fn core_data_types() {
     let _x = 0; // unused variable, if the above allow dead_code 
                     // statement was not here, then the compiler would give 
                     // warning
@@ -50,5 +50,48 @@ fn main() {
     // boolean
     let g: bool = false;
     println!("Value of g {},  it size is {} byte(s)", g, mem::size_of_val(&g));
-    
+}
+
+fn operators() {
+    let mut a = 2 + 3 * 4;
+    println!("{}", a);
+
+    a = a + 1;
+    a += 1; // ++ or -- operators do not exist in Rust
+
+    // also there is no power operator, you need to use the build in function
+    let a_cubed: i32 = i32::pow(a, 3);
+    println!("{}", a_cubed);
+
+    // in case you want to take the floating number power of another number
+    // you need to use some approximation
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi: f64 = f64::powf(b, std::f64::consts::PI);
+    println!("{} cubed = {}, {}^pi = {}",b , b_cubed, b, b_to_pi);
+
+    // bitwise operators
+
+    let c = 1 | 2;
+    println!("1|2 = {}", c);
+
+    let two_to_10 = 1 << 10; // left shift multiplies by 2^n;
+    println!("2^n = {}", two_to_10);
+
+    // logical operators
+
+    let pi_less_4 = std::f64::consts::PI < 4.0; // true
+    let v = 5;
+    let v_is_5 = v == 5;
+
+    println!("Pi is less then 4 = {}", pi_less_4);
+    println!("v is equal to 5 = {}", v_is_5);
+
+
+}
+
+fn main() {
+    core_data_types();
+    operators();
 }
